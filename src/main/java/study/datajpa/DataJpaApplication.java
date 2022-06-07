cb4c5679371f6948d56708204dc2bfc3b2c190d5
kangyongbong <rkdqhd11@hanmail.net>
2022-06-07 17:09:33 +0900

    인터페이스

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository") - boot는 없어도 됨 ,base packge와 repository 경로가 다르면 필요함
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
