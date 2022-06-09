e414ec95986b44f670beb6b8ab6ba96f2c5f33b7
kangyongbong <rkdqhd11@hanmail.net>
2022-06-09 13:25:05 +0900

    namedquery와 query annotation

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+//@NamedQuery(
+//        name="Member.findByUsername",
+//        query="select m from Member m where m.username = :username")
+// 실무에서 쓰이지 않음
 public class Member {
 
     @Id
@@ -32,6 +36,11 @@ public class Member {
         if (team != null) {
             changeTeam(team);
         }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
     }
 
     public void changeTeam(Team team) {
