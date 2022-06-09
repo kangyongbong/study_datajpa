dec0f742a8fea32058ae47e65b6be996d70c6c43
kangyongbong <rkdqhd11@hanmail.net>
2022-06-09 16:37:27 +0900

    반환 타입 및 페이징

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,18 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -18,4 +25,33 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username= :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findByUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    // 다양한 반환타입 가능
+    List<Member> findListByUsername(String name); //컬렉션
+    Member findMemberByUsername(String name); //단건
+    Optional<Member> findOptionalByUsername(String name); //단건 Optional
+
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    @Query(value = "select m from Member m left outer join m.team t where m.age = :age"
+            , countQuery = "select count(m) from Member m")
+        // count 분리 가능
+    Page<Member> findByAgeQuery(@Param("age") int age, Pageable pageable);
+
+    Slice<Member> findSliceByAge(int age, PageRequest pageRequest);
+
+    List<Member> findTop3ByAge(int age);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,11 +3,19 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +25,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -90,5 +100,98 @@ public class MemberRepositoryTest {
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(10);
         assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(teamA);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void resultType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        Member m3 = new Member("CCC", 30);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        memberRepository.save(m3);
+
+        // 없으면 null , 2건이상이면 NonUniqueResultException -> spring exception 으로 변경 incorrectResultSizeDataAccessException (Db 에 따른 수정이 필요없음 )
+        Member member = memberRepository.findMemberByUsername("AAA");
+        // 데이터가 없으면 null 이 아니라 빈 Collection 반환 , null 이 아닌게 보장
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        // 없으면 optional empty
+        Optional<Member> optional = memberRepository.findOptionalByUsername("FFF");
+        Member member1 = optional.orElse(m2);
+        Member member2 = optional.orElseGet(() -> memberRepository.findMembers("CCC"));
+        System.out.println("member1 = " + member1);
+        System.out.println("member2 = " + member2);
+    }
+
+    @Test
+    public void page() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        //when
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+        Page<Member> page = memberRepository.findByAge(10, pageRequest); // 반환타입이 page 이면 totalCount 를 자동으로 실행
+        Page<Member> page2 = memberRepository.findByAgeQuery(10, pageRequest); // count query 분리
+
+        //then
+        List<Member> content = page.getContent(); //조회된 데이터
+        assertThat(content.size()).isEqualTo(3); //조회된 데이터 수
+        assertThat(page.getTotalElements()).isEqualTo(5); //전체 데이터 수
+        assertThat(page.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+        assertThat(page.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+        Page<MemberDto> dtoPage = page.map(m -> new MemberDto()); // 생성자에서 특정 값만 넘겨도 가능, page 는 api 로 넘기면 json으로 count 등 page 정보가 자동으로 매핑된다
+
+        List<Member> content2 = page2.getContent(); //조회된 데이터
+        assertThat(content.size()).isEqualTo(3); //조회된 데이터 수
+        assertThat(page2.getTotalElements()).isEqualTo(5); //전체 데이터 수
+        assertThat(page2.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(page2.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+        assertThat(page2.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(page2.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+        Slice<Member> slice = memberRepository.findSliceByAge(10, pageRequest); // slice는 totalCount가 필요없어서 실행하지 않음 , size +1 개 로딩
+        List<Member> content3 = slice.getContent(); //조회된 데이터
+        assertThat(content3.size()).isEqualTo(3); //조회된 데이터 수
+        assertThat(slice.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(slice.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(slice.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+        List<Member> top3ByAge = memberRepository.findTop3ByAge(10);
+        assertThat(top3ByAge.size()).isEqualTo(3);
     }
 }
